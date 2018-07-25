c51149c277b364a8c85a8f274063b37292cea0f0
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-25 06:13:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -17,9 +17,10 @@ def hopper
      }
      
 programmer_hash[:grace_hopper]
+binding.pry
 end
 
-binding.pry
+
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
